4411f5d85482776ed2cec14bf7cb7fbdf55165b1
YS <yogendrasingh890@gmail.com>
2018-03-03 03:07:45 +0530

    some enhancements.

--- DieRollingSim.py
+++ DieRollingSim.py
@@ -1,10 +1,12 @@
 from random import randint
 
-print("\nPress Enter to Roll the dice\n")
+import sys
+
+print("\nPress 1 to Roll the dice\n")
 x=input()
-while (ord('x')==13):
+while x=='1':
     print(randint(1, 6))
-    print("\n\nPress ENTER to Roll the dice again and ESC to end\n")
+    print("\n\nPress 1 to Roll the dice again and 0 to end\n")
     x=input()
-    if (ord('x')==27):
-        break
+    if ('x'=='0'):
+        sys.exit
