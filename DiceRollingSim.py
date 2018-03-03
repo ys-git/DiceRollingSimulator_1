ccce9f4fda4e3071265d19ee40662450bbdabdff
YS <yogendrasingh890@gmail.com>
2018-03-03 23:11:02 +0530

    some changes.

--- DiceRollingSim.py
+++ DiceRollingSim.py
@@ -1,15 +1,35 @@
 import sys
 
 from random import randint
+def inp():
+    x=input()
+    return x
+def chkl():
+    while l!='1' or l!='0':
+        print("Please Enter the relevant input")
+        l = inp()
+
+
 print("\nPress 1 to Roll the dice\n")
-x=input()
-while x=='1':
-    print(randint(1, 6))
-    while x=='1' or x=='0':
-        print("\n\nPress 1 to Roll the dice again and 0 to end\n")
-        x = input()
-        if ('x' == '0'):
-            sys.exit1
-        else:
-            print("Please Enter the relevent input")
+l=inp()
+
+
+
+
+while l=='1' or l=='0':
+    if l=='1':
+        print(randint(1, 6))
+        print("\n\nPress 1 to Roll the dice again and 0 to end •\n")
+        l = inp()
+    elif ('l' == '0'):
+        sys.exit1
+    else :
+        chkl()
+
+
+
+
+
+
+
 
