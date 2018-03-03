53b3d7526e113c5cd6af3cda296d090e2d5ea316
YS <yogendrasingh890@gmail.com>
2018-03-03 21:26:14 +0530

    some enhancements.

--- DiceRollingSim.py
+++ DiceRollingSim.py
@@ -5,7 +5,11 @@ print("\nPress 1 to Roll the dice\n")
 x=input()
 while x=='1':
     print(randint(1, 6))
-    print("\n\nPress 1 to Roll the dice again and 0 to end\n")
-    x=input()
-    if ('x'=='0'):
-        sys.exit
+    while x=='1' or x=='0':
+        print("\n\nPress 1 to Roll the dice again and 0 to end\n")
+        x = input()
+        if ('x' == '0'):
+            sys.exit1
+        else:
+            print("Please Enter the relevent input")
+
